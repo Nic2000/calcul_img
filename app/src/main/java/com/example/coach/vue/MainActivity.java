31ca7ebea02dc0c851daa1fe81fdcb043457cc77
Nic Rah <nicrah16@gmail.com>
2021-09-11 18:34:40 +0300

    test evenement bouton

--- app/src/main/java/com/example/coach/vue/MainActivity.java
+++ app/src/main/java/com/example/coach/vue/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.coach.vue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.coach.R;
+import com.example.coach.controleur.Controle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        this.controle = Controle.getInstance();
     }
     private EditText txtPoids;
     private EditText txtTaille;
@@ -23,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton rdHomme;
     private TextView lblResultat;
     private ImageView imgSmiley;
-
+    private Controle controle;
     /**
      * initialisation des liens avec les objets graphiques
      */
@@ -34,5 +39,17 @@ public class MainActivity extends AppCompatActivity {
         rdHomme = (RadioButton) findViewById(R.id.rdHomme);
         lblResultat = (TextView) findViewById(R.id.lblResultat);
         imgSmiley = (ImageView) findViewById(R.id.imgSmiley);
+        ecouteCalcul();
+    }
+
+    /**
+     * Ecoute évènement sur le bouton calcul
+     */
+    private void ecouteCalcul(){
+        ((Button) findViewById(R.id.btnCalcul)).setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"test",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
